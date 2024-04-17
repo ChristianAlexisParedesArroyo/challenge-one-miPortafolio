b4412ab94f27b359f2ec5b241457f4f8f05ebb80
Christian Paredes <paredes.christian78@outlook.com>
2024-04-16 23:09:13 -0600

    implementacion de validacion

--- validacion.js
+++ validacion.js
@@ -1,1 +1,43 @@
-//Haz tú validación en javascript acá
+//validación en javascript
+
+
+function validarForm(){
+
+    var expRegNombre=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    var expRegCorreo=/^\w+@(\w+\.)+\w{2,4}$/; 
+  
+  
+    var nombre = document.getElementById("nombre");
+    var correo = document.getElementById("correo");
+
+    //Campo nombre
+    if(!nombre.value)
+    {
+    alert("El campo nombre es requerido");
+    nombre.focus();
+    return false;
+    }
+    if (!expRegNombre.exec(nombre.value))
+    {
+        alert("El campo nombre solo admite letras y espacios.")
+        nombre.focus();
+        return false;
+    }
+       
+  
+       //campo email
+       if(!correo.value)
+       {
+        alert("El campo correo es requerido");
+        correo.focus();
+        return false;
+       }
+       if(!expRegCorreo.exec(correo.value))
+       {
+         alert("El campo correo no tiene el formato correcto.")
+         correo.focus();
+         return false;
+       }
+ 
+}
+    
